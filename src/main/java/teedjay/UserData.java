0da7346c49a865102b2f2009224cb27e8c7d08fc
Thore Johnsen <thore.johnsen@unit4.com>
2015-02-10 13:47:14 +0100

    The example is now complete and runs using Java 8 and Jetty 8.

---
+++ src/main/java/teedjay/UserData.java
@@ -0,0 +1,38 @@
+package teedjay;
+
+/**
+ * A simple data structure - contained in a POJO
+ *
+ * @author thore
+ */
+public class UserData {
+
+    private String name;
+    private String city;
+    private String code;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+}
